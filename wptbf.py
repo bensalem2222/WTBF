2eb11060eb2afc0c1dca597923161ed7d1f774fe
Nabil tn <42371795+bensalem2222@users.noreply.github.com>
2018-08-15 05:34:38 -0700

    Add files via upload

---
+++ wptbf.py
@@ -0,0 +1,19 @@
+import re
+ 
+print (ga.green+'''
+        __          __  _     _____                 ____       
+        \ \        / / | |   |  __ \               |___ \      
+         \ \  /\  / /__| |__ | |__) |_      ___ __   __) |_ __ 
+          \ \/  \/ / _ \ '_ \|  ___/\ \ /\ / / '_ \ |__ <| '__|
+           \  /\  /  __/ |_) | |     \ V  V /| | | |___) | |   
+            \/  \/ \___|_.__/|_|      \_/\_/ |_| |_|____/|_|   
+                                                    
+        ##############################################################
+        #| "WebPwn3r" Web Applications Security Scanner              #
+        #|  By Ebrahim Hegazy - @Zigoo0                              #
+        #|  This Version Supports Remote Code/Command Execution, XSS #
+        #|  And SQL Injection.                                       #
+        #|  Thanks @lnxg33k, @dia2diab @Aelhemily, @okamalo          #
+        #|  More Details: http://www.sec-down.com/wordpress/?p=373   #
+        ##############################################################
+        '''+ga.end)
